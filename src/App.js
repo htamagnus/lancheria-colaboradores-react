22f3fc2ec0fa2234eeb8264a39126ac2cff7f926
Ágatha <agathamagnusg@hotmail.com>
2024-06-05 16:07:31 -0300

    ⬆️ update app

--- src/App.js
+++ src/App.js
@@ -3,16 +3,12 @@ import LoginPage from './pages/Login/LoginPage'
 import RegisterPage from './pages/Register/RegisterPage'
 import './styles/stylesGlobals.scss'
 import ProfilePage from './pages/Profile/ProfilePage'
-import CartPage from './pages/Cart/CartPage'
-import PaymentPage from './pages/Payment/PaymentPage'
-import OrderPage from './pages/Order/OrderPage'
-import TrackOrderPage from './pages/TrackOrder/TrackOrder'
-import FeedbackPage from './pages/Feedback/FeedbackPage'
-import NotificationPage from './pages/Notification/NotificationPage'
-import LocationPage from './pages/Location/LocationPage'
 import HomePage from './pages/Home/HomePage'
-import SocialPage from './pages/Social/SocialPage'
 import SupportPage from './pages/Support/SupportPage'
+import DeliveriesPage from './pages/Deliveries/DeliveriesPage'
+import OrderManagementPage from './pages/OrderManagement/OrderManagementPage'
+import StockPage from './pages/Stock/StockPage'
+import ChatPage from './pages/Chat/ChatPage'
 
 export default function App() {
   return (
@@ -20,15 +16,11 @@ export default function App() {
       <Route path="/" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/profile" element={<ProfilePage />} />
-      <Route path="/cart" element={<CartPage />} />
-      <Route path="/payment" element={<PaymentPage />} />
-      <Route path="/order" element={<OrderPage />} />
-      <Route path="/track-order" element={<TrackOrderPage />} />
-      <Route path="/feedback" element={<FeedbackPage />} />
-      <Route path="/notification" element={<NotificationPage />} />
-      <Route path="/location"element={<LocationPage />} />
+      <Route path="/chat" element={<ChatPage />} />
+      <Route path="/stock" element={<StockPage />} />
+      <Route path="/deliveries" element={<DeliveriesPage />} />
+      <Route path="/order-management" element={<OrderManagementPage />} />
       <Route path="/home"element={<HomePage />} />
-      <Route path="/social"element={<SocialPage />} />
       <Route path="/support"element={<SupportPage />} />
     </Routes>
   )
