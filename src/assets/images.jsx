54a1f9ee57d149f834941243e26b47a4da4aa3ee
Ágatha <agathamagnusg@hotmail.com>
2024-06-05 16:06:59 -0300

    ✨ update export file

--- src/assets/images.jsx
+++ src/assets/images.jsx
@@ -1,29 +1,13 @@
-import logo from './logo-colorida.png';
-import sandwich from './images/sandwich.png';
-import drinks from './images/drinks.png';
-import toppings from './images/toppingsCheesecake.png';
-import combos from './images/combos.png';
-import salad from './images/salad.png';
-import cakes from './images/cakes.png';
-import sandwichPromo from './images/sandwich-promo.png';
-import drinksPromo from './images/drinks-promo.png';
-import toppingsPromo from './images/toppings-promo.png';
-import combosPromo from './images/combos-promo.png';
-import saladPromo from './images/salad-promo.png';
-import cakesPromo from './images/cakes-promo.png';
+import potato from './images/potato.png';
+import tomato from './images/tomato.png';
+import lettuce from './images/lettuce.png';
+import cheese from './images/cheese.png';
+import onion from './images/onion.png';
 
 export {
-  logo,
-  sandwich,
-  drinks,
-  toppings,
-  combos,
-  salad,
-  cakes,
-  sandwichPromo,
-  drinksPromo,
-  toppingsPromo,
-  combosPromo,
-  saladPromo,
-  cakesPromo
+  potato,
+  tomato,
+  lettuce,
+  cheese,
+  onion
 };
